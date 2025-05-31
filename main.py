1e6ba81d16dc35f64f8e2ad07cee5ffcf6aaa26c
jesurodriguezg <jesusrcarcks@gmail.com>
2025-05-31 12:51:57 -0500

    reajuste

--- main.py
+++ main.py
@@ -11,24 +11,23 @@ numero2 = float(input("Introduce el segundo número: "))
 
 
 #Con este comando la calculadora para a pedir el ingreso de la operación
-operacion = int("Selecciona la operación que deseas realizar: \n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n")
+n = int("Selecciona la operación que deseas realizar: \n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n")
 
 
 
-
-# Con estos comandos la calculadora realiza las operaciones
-if operacion == 3:
-    resultado = numero1 * numero2
-elif operacion == 4:
-    if numero2 != 0:
-        resultado = numero1 / numero2
-    else:
-        resultado = "Error: División por cero no permitida"
-
-
-
+def validacion(n):
+    # Con estos comandos la calculadora realiza las operaciones
+    if n == 1:
+        return numero1 + numero2
+    elif n == 2:
+        return numero1 - numero2
+    elif n == 3:
+        return numero1 * numero2
+    elif n == 4:
+        return numero1 / numero2
 
 
 
-# Imprime el resultado
-print("El resultado de la operación es:", resultado)
+def main():
+    # Imprime el resultado
+    print("El resultado de la operación es:", validacion(n))
